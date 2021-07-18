1aa00d095f75f8cd17f7091bc7ce052e14528b8d
Marnu Lombard <marnulombard@gmail.com>
2021-07-18 13:23:07 +0200

    [create] Add cast to turn json object into a `ColliverySettingsCollection`

---
+++ app/Casts/ColliverySettingsCollectionCast.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ShopifyPlugin\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+use ShopifyPlugin\ColliveryApi\ColliverySettingsCollection;
+
+class ColliverySettingsCollectionCast implements CastsAttributes
+{
+
+    /**
+     * @param Model                       $model
+     * @param ColliverySettingsCollection $value
+     *
+     * @throws \JsonException
+     */
+    public function get($model, string $key, $value, array $attributes): ColliverySettingsCollection
+    {
+        if (!$value) {
+            return ColliverySettingsCollection::fromDefault();
+        }
+
+        $settings = json_decode($value, false, 3, JSON_THROW_ON_ERROR);
+
+        return ColliverySettingsCollection::fromDatabase($settings);
+    }
+
+    /**
+     * @param Model                       $model
+     * @param ColliverySettingsCollection $value
+     *
+     * @throws \JsonException
+     */
+    public function set($model, string $key, $value, array $attributes): array
+    {
+        return [$key => $value->toDatabase()];
+    }
+}
