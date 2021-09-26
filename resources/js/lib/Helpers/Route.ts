fd8b4dd40450815e550a728386354a548c2b90b4
Marnu Lombard <marnulombard@gmail.com>
2021-09-26 14:49:26 +0200

    [change] Updates the params to `ziggy`'s `route()` - Was changed in a library night

--- resources/js/lib/Helpers/Route.ts
+++ resources/js/lib/Helpers/Route.ts
@@ -1,7 +1,7 @@
-import {Config, default as ziggyRoute, InputParams} from 'ziggy-js'
+import {Config, default as ziggyRoute, RouteParamsWithQueryOverload} from 'ziggy-js'
 import {Ziggy} from '../../routes'
 
-const route = (name: string, params?: InputParams, absolute?: boolean) => ziggyRoute(name, params, absolute, Ziggy as unknown as Config);
+const route = (name: string, params?: RouteParamsWithQueryOverload, absolute?: boolean) => ziggyRoute(name, params, absolute, Ziggy as unknown as Config);
 
 window.route = route;
 
