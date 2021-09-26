f3e2763b8328796c4f0073ef01b6bdcdc1709c8c
Marnu Lombard <marnulombard@gmail.com>
2021-09-26 14:26:14 +0200

    [change] Convert the waybill image to jpg in order to display it in the browser

--- app/ColliveryApi/ApiRequests/WaybillDocumentApiRequest.php
+++ app/ColliveryApi/ApiRequests/WaybillDocumentApiRequest.php
@@ -11,9 +11,28 @@ class WaybillDocumentApiRequest extends ApiRequest
     /**
      * @throws PropertyDoesNotExist
      * @throws HttpClientException
+     * @throws \ImagickException
      */
-    public function waybill(int $waybillId)
+    public function waybill(int $waybillId): WaybillDocument
     {
-        return WaybillDocument::fromResponse($this->get("waybill_documents/$waybillId/waybill")->data);
+        $response = $this->get("waybill_documents/$waybillId/waybill");
+        $waybillDocument = WaybillDocument::fromResponse($response->data);
+
+        $image = new \Imagick();
+        $image->setResolution(200, 200);
+        $image->setCompression(\Imagick::COMPRESSION_LOSSLESSJPEG);
+        $image->setCompressionQuality(80);
+        // Now load the pixels
+        $image->readImageBlob(base64_decode($waybillDocument->image));
+        $image->stripImage();
+
+        $image->setImageResolution(200, 200);
+        $image->setFormat('jpg');
+        $image->setImageFormat('jpg');
+        $image->setImageFilename("$waybillId.jpg");
+
+        $waybillDocument->image = base64_encode($image->__toString());
+
+        return $waybillDocument;
     }
 }
