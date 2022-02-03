2ad3eb8ebd5501a4567cc71c1135dbfe0e9cd79f
Marnu Lombard <marnulombard@gmail.com>
2022-02-03 08:50:07 +0200

    [change] Add missing data to the `ExtendedAttributes` DTO

--- app/ShopifyApi/Models/ExtendedAuthorizationAttributes.php
+++ app/ShopifyApi/Models/ExtendedAuthorizationAttributes.php
@@ -9,4 +9,6 @@ use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 #[TypeScript]
 class ExtendedAuthorizationAttributes extends DataTransferObject
 {
+    public string $extended_authorization_expires_at;
+    public string $standard_authorization_expires_at;
 }
